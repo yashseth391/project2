8677b0ebfb3b13463c24167176924a81cec684a7
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-04-28 23:19:28 +0530

    started again

---
+++ AppPro.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 
+{View,
+    Text
+
+} from 'react-native'
+
+function AppPro()
+{
+    return(
+        <View>
+            <Text>HELLO</Text>
+        </View>
+    )
+}
+
+export default AppPro
