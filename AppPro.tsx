0b12777f2de09cb644e03d11341a2b2ffdb4360c
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-05-26 23:44:24 +0530

    first complete

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from 'react' ;
 import 
-{View,
-    Text
+{   View,
+    Text,
+    StyleSheet,
+    useColorScheme
 
-} from 'react-native'
+} from 'react-native' ;
 
-function AppPro()
+function AppPro() :JSX.Element
 {
+    const isdarkmode=useColorScheme()=='dark'
+    const styles= StyleSheet.create(
+        {
+            container : {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            whiteText : {
+                color: 'white',
+            },
+            darkText : {
+                color :'blue',
+            }
+        }
+    );
     return(
-        <View>
-            <Text>HELLO</Text>
+        <View style={styles.container}>
+            <Text style={isdarkmode ? styles.darkText :styles.whiteText }>HELLO</Text>
         </View>
     )
 }
-
 export default AppPro
