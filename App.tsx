9511aa6e4c22ef79ef8be587fc9fe73a483e1859
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-05-29 00:59:35 +0530

    default look

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 import React from 'react';
+import Flatcards from './components/Flatcards';
 
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>   
-        <Text>App</Text>
-        <Flatcards></Flatcards>
+        <Flatcards/>
       </ScrollView>
+      
     </SafeAreaView>
   );
 };
---
+++ components/Flatcards.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Flatcards = () => {
+  return (
+    <View>
+      <Text style={styles.heading}>Flatcards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card,styles.cardone]}>
+          <Text >Red</Text>
+        </View>
+        <View style={[styles.card,styles.cardtwo]}>
+          <Text >Green</Text>
+        </View>
+        <View style={[styles.card,styles.cardthree]}>
+          <Text >blue</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default Flatcards
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize:32,
+    fontWeight:"bold",
+    paddingHorizontal:10,
+  },
+  container : {
+
+  },
+  card:{
+    flex:1,
+   alignItems:'center',
+   justifyContent:'center',
+      width:100,
+      height:100,
+      borderRadius:5,
+      margin:10,
+  },
+  cardone: {
+     backgroundColor:'red',
+  },
+  cardtwo:{
+    backgroundColor:'green',
+  },
+  cardthree:{
+    backgroundColor:'blue',
+  }
+})
